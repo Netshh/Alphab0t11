9d06d2b45ab9333fe4b65dcff542e01f18f11a97
Netshh <93586570+Netshh@users.noreply.github.com>
2021-12-08 14:16:47 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -1,27 +1,16 @@
 const donasibot = () => {
-return `DONASI BISA VIA BANK / E-MONEY
+return `DONASI BISA VIA :
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
 	
 *💰 E-MONEY*
-
 1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
-3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0896 6264 3883
+2. DANA
+	ㅁ 0896 6264 3883
 	
-Semuanya atas nama RI****DI
+*📲 PULSA*
+1. Tri
+	ㅁ 0896 6264 3883
 `
 	}
 
